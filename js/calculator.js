99f3684a7adf604bb48268a3e33d0c80d826e05b
Samuel Henry Kurniawan <kurniawans@sea.com>
2022-02-11 17:00:41 +0800

    review

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,16 @@
+// First of all, formattingnya beneran perlu dibenerin, ada yang pake semicolon ada yang engga gitu lumayan ga enak dibacanya.
+// tiba tiba ada else statement else {} yang isinya di line yang sama juga dibacanya sakit
+// terus tiba tiba ada else yang ga pake {} langsung inline itu juga dibacanya sakit
+// Bisa consider pake this tool: https://prettier.io/
+
+
 class Calculator {
+    // equation dan answer probably a better naming dari upperline dan lowerline
     constructor(upperline, lowerline) {
+        // leftHandInput mungkin lebih enak dibaca
         this.lefthandinput = upperline[0]
         this.oper = upperline[1]
+        // rightHandInput mungkin lebih enak dibaca
         this.righthandinput = upperline[2]
 
         this.lowerline = lowerline;
@@ -16,23 +25,38 @@ class Calculator {
         this.lowerline.innerText = "";
     }
 
+    // javascript conventionnya camelCase bukan snake_case
     append_number(number) {
+        // Generally structure
+        // if (!x) {
+        // } else {
+        // }
+        // tidak direkomendasikan, prefer structure
+        // if (x) {
+        // } else {
+        // }
+        // Human brain lebih gampang baca kalau ga dinegasi, gunakan negasi kalau bener2 diperlukan aja
         if (!this.oper.innerText) {
             if (["+", "-", "x", "÷"].includes(number)) {
                 if (this.lefthandinput.innerText) {
                     this.oper.innerText = number;
+                //unneccessary if
                 } else if (!this.lefthandinput.innerText) {
                     return
                 }
+            // avoid == kalo javascript, selalu pake === (coba cek apa bedanya)
             } else if (number == ".") {
                 if (!this.lefthandinput.innerText.includes(".")) {
                     this.lefthandinput.append(".")
                 } else return;
+            // tanda komamya salah tempat.....
             } else {this.lefthandinput.append(number)};
+            // unneccessary if, kalo ga !this.oper.innerText berarti this.oper.innerText kan....
         } else if (this.oper.innerText) {
             if (["+", "-", "x", "÷"].includes(number)) {
                 return
             } else if (number == ".") {
+                // Duplicated, mungkin if else condition structurenya perlu dibenerin lagi
                 if (!this.righthandinput.innerText.includes(".")) {
                     this.righthandinput.append(".")
                 } else return;
@@ -51,6 +75,8 @@ class Calculator {
     }
 
     compute() {
+        // lots of duplicated statement, mungkin calculate answernya bisa dilakukan in a separate function
+        // Number(this.lefthandinput.innerText) sama yang righthandinputnya bisa diekstrak ke separate variable
         if (this.oper.innerText == "+") {
             let answer = Number(this.lefthandinput.innerText) + Number(this.righthandinput.innerText);
             this.lowerline.innerText = answer;
@@ -67,6 +93,7 @@ class Calculator {
     }
 }
 
+// Im not sure kenapa ini semua pake attribute ya, mending pake class atau id ga sih
 const numberButton = document.querySelectorAll('[number]');
 const operandButton = document.querySelectorAll('[operand]');
 const clearButton = document.querySelector('[clear]');
